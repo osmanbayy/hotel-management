33931922e7c450ff71ee8e0d2759cc42a2347f93
Osman Bay <bayosman342@gmail.com>
2025-04-13 23:12:37 +0300

    font size and modal size updated

--- client/src/components/FooterColumn.jsx
+++ client/src/components/FooterColumn.jsx
@@ -3,11 +3,11 @@ import React from "react";
 export default function FooterColumn({ title, items }) {
   return (
     <div className="flex flex-col gap-3">
-      <h1 className="font-bold tracking-wide">{title}</h1>
+      <h1 className="font-semibold md:font-bold md:tracking-wide">{title}</h1>
       {items.map((item, idx) => (
         <p
           key={idx}
-          className="text-gray-500 text-md md:font-semibold md:tracking-wide cursor-pointer"
+          className="text-gray-500 text-xs md:text-md md:font-semibold md:tracking-wide cursor-pointer"
         >
           {item}
         </p>
